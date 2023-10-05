21276a6c199683087cd66f0cc4d2987291ea19e6
KannanMaruthappan <kannan.maruthappan@gmail.com>
2023-10-05 16:36:35 +0100

    driver local reference

--- Demo.py
+++ Demo.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 import time
 
 print("#### Starting ####")
-driver = webdriver.Chrome()
+service = Service(executable_path='./Browser/chromedriver.exe')
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
 driver.maximize_window()  
 # driver.get("https://www.google.com/")
 driver.get("file:///C:/Productivity/GIT/PySelenium/index.html")
